fde4c83f5d853de607c7ecae326e0e190882e8c9
salma-sayed1910 <salma.sayed@bulme.at>
2025-12-04 16:54:10 +0100

    added green button

--- PlatformioDevrim/arduino-simon-game-main/src/main.cpp
+++ PlatformioDevrim/arduino-simon-game-main/src/main.cpp
@@ -1,53 +1,59 @@
-
 #include <Arduino.h>
-//Bounce 2 Library importieren 
 #include <Bounce2.h>
-
-//buttons an den Pins 2 & 3
-//#define BTN_YLW 2
-//#define BTN_BlU 3
 
 const int BTN_YLW = 2;
 const int BTN_BlU = 3;
+const int BTN_GRN = 4;
 
 const int LED_GRN = 8;
 const int LED_PNK = 9;
 
-Bounce2::Button btnYellow = Bounce2::Button(); 
-Bounce2::Button btnBlue = Bounce2::Button(); 
+Bounce2::Button btnYellow = Bounce2::Button();
+Bounce2::Button btnBlue = Bounce2::Button();
+Bounce2::Button btnGruen = Bounce2::Button();
 
-int counter = 0; 
+void setup() {
 
-void setup(){
+  Serial.begin(9600);
 
-  Serial.begin(9600); 
+  pinMode(LED_GRN, OUTPUT);
+  pinMode(LED_PNK, OUTPUT);
 
-  pinMode(LED_GRN,LOW); 
-  pinMode(LED_PNK,LOW); 
+  btnYellow.attach(BTN_YLW, INPUT_PULLUP);
+  btnYellow.setPressedState(LOW);
 
-  btnYellow.attach(BTN_YLW, INPUT_PULLUP); //Hier button auf pin 2 zuordnen
-  btnYellow.setPressedState(LOW); //Button ist Active-Low
+  btnBlue.attach(BTN_BlU, INPUT_PULLUP);
+  btnBlue.setPressedState(LOW);
 
-  btnBlue.attach(BTN_BlU,INPUT_PULLUP);
-  btnBlue.setPressedState(LOW); 
-
+  btnGruen.attach(BTN_GRN, INPUT_PULLUP);
+  btnGruen.setPressedState(LOW);
 }
 
-
 void loop() {
-  btnYellow.update(); 
-  btnBlue.update(); 
 
-  if(btnYellow.pressed())
-  {
-    Serial.println("Yellos has been pressed"); 
+  btnYellow.update();
+  btnBlue.update();
+  btnGruen.update();
 
-    digitalWrite(LED_GRN, HIGH); 
-    delay(2000); 
-    digitalWrite(LED_GRN, LOW); 
+  if (btnYellow.pressed()) {
+    Serial.println("Yellow has been pressed");
+    digitalWrite(LED_GRN, HIGH);
+    delay(2000);
+    digitalWrite(LED_GRN, LOW);
   }
-  if(btnBlue.pressed())
-  {
-    Serial.println("Blue has been pressed"); 
+
+  if (btnBlue.pressed()) {
+    Serial.println("Blue has been pressed");
+  }
+
+  if (btnGruen.pressed()) {
+
+    for (int i = 0; i < 5; i++) 
+    {
+      digitalWrite(LED_GRN, HIGH);
+      delay(300);
+      digitalWrite(LED_GRN, LOW);
+      delay(300);
+    }
   }
 }
