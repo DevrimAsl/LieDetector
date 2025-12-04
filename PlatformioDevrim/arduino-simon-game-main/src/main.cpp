4f4b72bb33387b9d94032378e61d16f1bdc5493d
Devrim Aslan <devrim.aslan@bulme.at>
2025-12-04 16:58:54 +0100

    changed button yellow

--- PlatformioDevrim/arduino-simon-game-main/src/main.cpp
+++ PlatformioDevrim/arduino-simon-game-main/src/main.cpp
@@ -36,14 +36,38 @@ void loop() {
   btnGruen.update();
 
   if (btnYellow.pressed()) {
-    Serial.println("Yellow has been pressed");
-    digitalWrite(LED_GRN, HIGH);
-    delay(2000);
-    digitalWrite(LED_GRN, LOW);
-  }
+    // S = 3x kurz
+    for (int i = 0; i < 3; i++)
+    {
+        digitalWrite(LED_PNK, HIGH);
+        delay(300);          // kurz
+        digitalWrite(LED_PNK, LOW);
+        delay(300);
+    }
 
-  if (btnBlue.pressed()) {
-    Serial.println("Blue has been pressed");
+    // kleine Pause zwischen Buchstaben
+    delay(600);
+
+    // O = 3x lang
+    for (int i = 0; i < 3; i++)
+    {
+        digitalWrite(LED_PNK, HIGH);
+        delay(900);          // lang
+        digitalWrite(LED_PNK, LOW);
+        delay(300);
+    }
+
+    // Pause zwischen Buchstaben
+    delay(600);
+
+    // S = 3x kurz
+    for (int i = 0; i < 3; i++)
+    {
+        digitalWrite(LED_PNK, HIGH);
+        delay(300);          // kurz
+        digitalWrite(LED_PNK, LOW);
+        delay(300);
+    }
   }
 
   if (btnGruen.pressed()) {
